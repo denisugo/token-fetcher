15f2e2d6b051410bc5e5b0da12cef33e64e4e9db
Denis Ustiuzhanin <superden1320@gmail.com>
2024-06-30 15:57:43 +0000

    feat: introduce jwtio redirect button

---
+++ components/JwtIoRedirectButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{ token: string }>();
+</script>
+<template>
+  <NuxtLink
+    :to="`https://jwt.io?token=${props.token}`"
+    target="_blank"
+    rel="noopener"
+    icon="pi pi-globe"
+    aria-label="Redirect"
+    label="open in jwt.io"
+  >
+    <Button label="open in jwt.io" class="w-10rem" />
+  </NuxtLink>
+</template>
--- pages/tokens.vue
+++ pages/tokens.vue
@@ -3,16 +3,12 @@ const { data: tokens } = await useFetch("/api/tokens");
 const accessToken = computed(() => tokens.value?.access_token);
 const idToken = computed(() => tokens.value?.id_token);
 const refreshToken = computed(() => tokens.value?.refresh_token);
-const tokenType = computed(() => tokens.value?.token_type);
-const expires = computed(() => tokens.value?.expires_in);
-
-function jwtDotIoRedirect(token: string) {
-  navigateTo(`https://jwt.io?token=${token}`, { external: true });
-}
+// const tokenType = computed(() => tokens.value?.token_type);
+// const expires = computed(() => tokens.value?.expires_in);
 </script>
 <template>
   <div>
-    <div v-if="accessToken" class="flex flex-column gap-2">
+    <div v-if="accessToken" class="flex flex-column gap-2 align-items-center">
       <label for="access-token">access token</label>
       <div class="card flex justify-content-center">
         <Textarea
@@ -23,28 +19,15 @@ function jwtDotIoRedirect(token: string) {
           disabled
         />
       </div>
-      <div class="w-10rem align-self-center">
-        <Button
-          icon="pi pi-globe"
-          aria-label="Submit"
-          label="open in jwt.io"
-          @click="jwtDotIoRedirect(accessToken)"
-        />
-      </div>
+      <JwtIoRedirectButton :token="accessToken" />
     </div>
     <div v-if="idToken" class="flex flex-column gap-2">
       <label for="id-token">id token</label>
       <div class="card flex justify-content-center">
         <Textarea id="id-token" v-model="idToken" rows="5" cols="30" disabled />
       </div>
-      <div class="w-10rem align-self-center">
-        <Button
-          icon="pi pi-globe"
-          aria-label="Submit"
-          label="open in jwt.io"
-          @click="jwtDotIoRedirect(idToken)"
-        />
-      </div>
+
+      <JwtIoRedirectButton :token="idToken" />
     </div>
     <div v-if="refreshToken" class="flex flex-column gap-2">
       <label for="refresh-token">refresh token</label>
@@ -57,14 +40,8 @@ function jwtDotIoRedirect(token: string) {
           disabled
         />
       </div>
-      <div class="w-10rem align-self-center">
-        <Button
-          icon="pi pi-globe"
-          aria-label="Submit"
-          label="open in jwt.io"
-          @click="jwtDotIoRedirect(refreshToken)"
-        />
-      </div>
+
+      <JwtIoRedirectButton :token="refreshToken" />
     </div>
     <div class="mt-2">
       <Button
