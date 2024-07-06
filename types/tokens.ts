dbf4e915ee8505f1ae06c9a60770daf769eaa416
Denis Ustiuzhanin <superden1320@gmail.com>
2024-07-06 16:30:32 +0000

    ref: remove unused types from TokensResponseDto

--- types/tokens.ts
+++ types/tokens.ts
@@ -2,8 +2,6 @@ export interface TokensResponseDto {
   access_token?: string;
   id_token?: string;
   refresh_token?: string;
-  expires_in?: number;
-  token_type?: string;
 }
 
 export interface TokensQueryDto {
