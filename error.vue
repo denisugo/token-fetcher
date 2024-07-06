102938b6330469b1ba19b13e51a38a47bfc51e7e
Denis Ustiuzhanin <superden1320@gmail.com>
2024-07-06 17:05:13 +0000

    feat: add default error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+import type { ErrorData } from "./types/error";
+
+const { error } = defineProps<{ error: NuxtError }>();
+</script>
+
+<template>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+    <h2>{{ error.message }}</h2>
+    <p>
+      {{ (JSON.parse(error.data as string) as ErrorData).originalErrorMessage }}
+    </p>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
