9c8b596ad7cb2a1d1b8efcd17f5d92024f92b7af
Denis Ustiuzhanin <superden1320@gmail.com>
2024-07-11 09:40:35 +0000

    feat: add copy button

---
+++ components/CopyButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useClipboard } from "@vueuse/core";
+
+const { source } = defineProps<{ source: string }>();
+
+const { copy } = useClipboard({ source });
+</script>
+
+<template>
+  <Button icon="pi pi-clipboard" label="copy" class="w-6rem" @click="copy()" />
+</template>
--- pages/tokens.vue
+++ pages/tokens.vue
@@ -25,7 +25,10 @@ const refreshToken = computed(() => query.refreshToken);
           />
         </div>
       </div>
-      <JwtIoRedirectButton :token="accessToken" />
+      <div class="flex gap-1 justify-content-center align-items-center w-full">
+        <JwtIoRedirectButton :token="accessToken" />
+        <CopyButton :source="accessToken" />
+      </div>
     </div>
 
     <div v-if="idToken" class="mb-2 flex flex-column gap-2 align-items-center">
@@ -41,7 +44,10 @@ const refreshToken = computed(() => query.refreshToken);
           />
         </div>
       </div>
-      <JwtIoRedirectButton :token="idToken" />
+      <div class="flex gap-1 justify-content-center align-items-center w-full">
+        <JwtIoRedirectButton :token="idToken" />
+        <CopyButton :source="idToken" />
+      </div>
     </div>
     <div
       v-if="refreshToken"
@@ -59,7 +65,11 @@ const refreshToken = computed(() => query.refreshToken);
           />
         </div>
       </div>
-      <JwtIoRedirectButton :token="refreshToken" />
+
+      <div class="flex gap-1 justify-content-center align-items-center w-full">
+        <JwtIoRedirectButton :token="refreshToken" />
+        <CopyButton :source="refreshToken" />
+      </div>
     </div>
     <div class="flex flex-column gap-2 align-items-center">
       <NuxtLink to="/"><Button label="Go Back" link /></NuxtLink>
