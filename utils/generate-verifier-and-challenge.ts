afc8e44492f72ac8820508af927a520396a3f2fe
Denis Ustiuzhanin <superden1320@gmail.com>
2024-08-10 12:36:09 +0200

    ref: rename verifier and challenge

--- utils/generate-verifier-and-challenge.ts
+++ utils/generate-verifier-and-challenge.ts
@@ -1,9 +1,9 @@
 import crypto from "crypto";
 
-export default function (): [verifier: string, challenge: string] {
-  const verifier = base64URLEncode(crypto.randomBytes(32));
-  const challenge = base64URLEncode(sha256(verifier));
-  return [verifier, challenge];
+export default function (): [codeVerifier: string, codeChallenge: string] {
+  const codeVerifier = base64URLEncode(crypto.randomBytes(32));
+  const codeChallenge = base64URLEncode(sha256(codeVerifier));
+  return [codeVerifier, codeChallenge];
 }
 
 // Stolen from https://auth0.com/docs/get-started/authentication-and-authorization-flow/authorization-code-flow-with-pkce/call-your-api-using-the-authorization-code-flow-with-pkce
